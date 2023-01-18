c543145cc58a14c4c9758731b45eb73f7482fd24
awinters81 <alexandria.winters81@gmail.com>
2023-01-17 20:31:40 -0500

    package.json and starting index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// I know that I'll need constants to pull from all in the lib folder, and Inquire.
+const manager = require("./lib/Manager");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const employee = require(".lib/Employee.js");
+
+// and I know I will need constants for Inquirer, the path, and fs...
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+// These will all render through my page.js in my src folder...
+
+const start = require("./src/page.js")
